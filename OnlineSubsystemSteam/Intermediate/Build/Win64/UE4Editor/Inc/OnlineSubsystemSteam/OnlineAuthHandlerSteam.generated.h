9410f3ed52f2329e00331b5c1ec8290bc1f5960f
uno1982 <jmanc82@aim.com>
2023-12-21 06:05:40 -0600

    Add Plugin

---
+++ OnlineSubsystemSteam/Intermediate/Build/Win64/UE4Editor/Inc/OnlineSubsystemSteam/OnlineAuthHandlerSteam.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ONLINESUBSYSTEMSTEAM_OnlineAuthHandlerSteam_generated_h
+#error "OnlineAuthHandlerSteam.generated.h already included, missing '#pragma once' in OnlineAuthHandlerSteam.h"
+#endif
+#define ONLINESUBSYSTEMSTEAM_OnlineAuthHandlerSteam_generated_h
+
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_SPARSE_DATA
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_RPC_WRAPPERS
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_RPC_WRAPPERS_NO_PURE_DECLS
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUSteamAuthComponentModuleInterface(); \
+	friend struct Z_Construct_UClass_USteamAuthComponentModuleInterface_Statics; \
+public: \
+	DECLARE_CLASS(USteamAuthComponentModuleInterface, UHandlerComponentFactory, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/OnlineSubsystemSteam"), NO_API) \
+	DECLARE_SERIALIZER(USteamAuthComponentModuleInterface)
+
+
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_INCLASS \
+private: \
+	static void StaticRegisterNativesUSteamAuthComponentModuleInterface(); \
+	friend struct Z_Construct_UClass_USteamAuthComponentModuleInterface_Statics; \
+public: \
+	DECLARE_CLASS(USteamAuthComponentModuleInterface, UHandlerComponentFactory, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/OnlineSubsystemSteam"), NO_API) \
+	DECLARE_SERIALIZER(USteamAuthComponentModuleInterface)
+
+
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USteamAuthComponentModuleInterface(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USteamAuthComponentModuleInterface) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USteamAuthComponentModuleInterface); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USteamAuthComponentModuleInterface); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USteamAuthComponentModuleInterface(USteamAuthComponentModuleInterface&&); \
+	NO_API USteamAuthComponentModuleInterface(const USteamAuthComponentModuleInterface&); \
+public:
+
+
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USteamAuthComponentModuleInterface(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USteamAuthComponentModuleInterface(USteamAuthComponentModuleInterface&&); \
+	NO_API USteamAuthComponentModuleInterface(const USteamAuthComponentModuleInterface&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USteamAuthComponentModuleInterface); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USteamAuthComponentModuleInterface); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USteamAuthComponentModuleInterface)
+
+
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_PRIVATE_PROPERTY_OFFSET
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_59_PROLOG
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_PRIVATE_PROPERTY_OFFSET \
+	DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_SPARSE_DATA \
+	DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_RPC_WRAPPERS \
+	DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_INCLASS \
+	DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_PRIVATE_PROPERTY_OFFSET \
+	DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_SPARSE_DATA \
+	DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_RPC_WRAPPERS_NO_PURE_DECLS \
+	DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_INCLASS_NO_PURE_DECLS \
+	DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h_62_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class SteamAuthComponentModuleInterface."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ONLINESUBSYSTEMSTEAM_API UClass* StaticClass<class USteamAuthComponentModuleInterface>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID DropIn_427_Plugins_OnlineSubsystemSteam_Source_Private_OnlineAuthHandlerSteam_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
